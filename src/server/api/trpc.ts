e1a4305a713ef0bab7b4bddead41f55e2efbd948
minaboktor <mina26b8@gmail.com>
2024-05-29 16:48:06 -0400

    added github integration and local db support

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -12,7 +12,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { getServerAuthSession } from "@/server/auth";
-import { db } from "@/server/db";
+import { prisma } from "@/lib/prisma";
 
 /**
  * 1. CONTEXT
@@ -30,7 +30,7 @@ export const createTRPCContext = async (opts: { headers: Headers }) => {
   const session = await getServerAuthSession();
 
   return {
-    db,
+    db: prisma,
     session,
     ...opts,
   };
